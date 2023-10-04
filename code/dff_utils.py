19eba132b9579820aa36d4a74fa2a864d16d100e
Arielle Leon <ariellel@alleninstitute.org>
2023-10-04 16:19:31 -0700

    fixing inputs

--- code/dff_utils.py
+++ code/dff_utils.py
@@ -1,5 +1,4 @@
 import os
-import re
 import argparse
 import json
 from pathlib import Path
@@ -26,13 +25,6 @@ def make_output_directory(output_dir: str, experiment_id: str=None) -> str:
         output_dir = os.path.join(output_dir)
     os.makedirs(output_dir, exist_ok=True)
     return output_dir
-
-
-def find_file(path, pattern):
-    for root, dirs, files in os.walk(str(path)):
-        for f in files:
-            if re.findall(pattern, f):
-                return Path(os.path.join(root, f))
             
             
 if __name__ == "__main__":
@@ -44,15 +36,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
     input_dir = Path(args.input_dir).resolve()
     output_dir = Path(args.output_dir).resolve()
-    neuropil_trace_h5_fp= find_file(input_dir, "neuropil_traces.h5")
-    experiment_id = find_file(input_dir, "\d{9}_registered.h5").name.split("_")[0]
+    neuropil_trace_fp = [i for  i in list(input_dir.glob("*/*")) if "neuropil_traces.h5" in str(i)][0]
+    motion_corrected_fn = [i for i in list(input_dir.glob("*/*")) if "registered.h5" in str(i)][0]
+    experiment_id = motion_corrected_fn.name.split("_")[0]
     output_dir = make_output_directory(output_dir, experiment_id)
-    process_json = find_file(input_dir, "processing.json")
+    process_json = [i for  i in list(input_dir.glob("*/*")) if "processing.json" in str(i)][0]
     with open(process_json, "r") as j:
         data = json.load(j)
     frame_rate = data["data_processes"][0]["parameters"]["movie_frame_rate_hz"]
     input_data = {
-        "input_file": str(neuropil_trace_h5_fp),
+        "input_file": str(neuropil_trace_fp),
         "output_file": str(output_dir / "dff.h5"),
         "movie_frame_rate_hz": frame_rate,
     }

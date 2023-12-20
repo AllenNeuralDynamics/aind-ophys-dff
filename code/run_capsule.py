096adbef5bb783af38877e616aef312d50c8ea63
Arielle Leon <ariellel@alleninstitute.org>
2023-12-19 20:56:42 -0800

    ready for testing

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -6,14 +6,70 @@ import shutil
 import aind_ophys_utils.dff as dff
 from scipy.stats import skew
 import h5py as h5
+import json
+from pathlib import Path
+from aind_data_schema.core.processing import Processing, DataProcess, ProcessName, PipelineProcess
+from typing import Union
+from datetime import datetime as dt
+from datetime import timezone as tz
 
 
-def make_output_directory(output_dir: str, experiment_id: str = None) -> str:
+def write_output_metadata(
+    metadata: dict,
+    process_name: str,
+    input_fp: Union[str, Path],
+    output_fp: Union[str, Path],
+    start_date_time: dt,
+) -> None:
+    """Writes output metadata to plane processing.json
+
+    Parameters
+    ----------
+    metadata: dict
+        parameters from suite2p motion correction
+    input_fp: str
+        path to data input
+    output_fp: str
+        path to data output
+    """
+    processing = Processing(
+        processing_pipeline=PipelineProcess(
+            processor_full_name="Multplane Ophys Processing Pipeline",
+            pipeline_url="https://codeocean.allenneuraldynamics.org/capsule/5472403/tree",
+            pipeline_version="0.1.0",
+            data_processes=[
+                DataProcess(
+                    name=process_name,
+                    software_version=os.getenv("AIND_OPHYS_UTILS_REPO_COMMIT_SHA"),
+                    start_date_time=start_date_time,  # TODO: Add actual dt
+                    end_date_time=dt.now(tz.utc),  # TODO: Add actual dt
+                    input_location=str(input_fp),
+                    output_location=str(output_fp),
+                    code_url=(os.getenv("AIND_OPHYS_UTILS_REPO_URL")),
+                    parameters=metadata,
+                )
+            ],
+        )
+    )
+    print(f"Output filepath: {output_fp}")
+    with open(Path(output_fp).parent.parent / "processing.json", "r") as f:
+        proc_data = json.load(f)
+    processing.write_standard_file(output_directory=Path(output_fp).parent.parent)
+    with open(Path(output_fp).parent.parent / "processing.json", "r") as f:
+        dct_data = json.load(f)
+    proc_data["processing_pipeline"]["data_processes"].append(
+        dct_data["processing_pipeline"]["data_processes"][0]
+    )
+    with open(Path(output_fp).parent.parent / "processing.json", "w") as f:
+        json.dump(proc_data, f, indent=4)
+
+
+def make_output_directory(output_dir: Path, experiment_id: str) -> str:
     """Creates the output directory if it does not exist
 
     Parameters
     ----------
-    output_dir: str
+    output_dir: Path
         output directory
     experiment_id: str
         experiment_id number
@@ -23,12 +79,12 @@ def make_output_directory(output_dir: str, experiment_id: str = None) -> str:
     output_dir: str
         output directory
     """
-    if experiment_id:
-        output_dir = os.path.join(output_dir, experiment_id)
-    else:
-        output_dir = os.path.join(output_dir)
-    os.makedirs(output_dir, exist_ok=True)
-    return Path(output_dir)
+    output_dir = output_dir / experiment_id
+    output_dir.mkdir(exist_ok=True)
+    output_dir = output_dir / "neuropil_correction"
+    output_dir.mkdir(exist_ok=True)
+
+    return output_dir
 
 
 def copy_data_to_results(input_dir: str, output_dir: str) -> None:
@@ -52,7 +108,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-o", "--output-dir", type=str, help="Output directory", default="/results/"
     )
-
+    start_time = dt.now(tz.utc)
     args = parser.parse_args()
     input_dir = Path(args.input_dir).resolve()
     output_dir = Path(args.output_dir).resolve()
@@ -71,5 +127,16 @@ if __name__ == "__main__":
         f.create_dataset("noise", data=noise)
         f.create_dataset("skewness", data=skewness)
         f.create_dataset("roi_names", data=roi_names)
+
+    write_output_metadata(
+        {
+            "skewness": skewness.tolist(),
+            "baseline": baseline.tolist(),
+            "noise": noise.tolist(),
+        },
+        ProcessName.DFF_ESTIMATION,
+        str(neuropil_trace_fp),
+        str(output_dir / "dff.h5"),
+        start_time,
+    )
     # This will be removed when I update the metadata and clean up the copy mess
-    copy_data_to_results(input_dir, output_dir)
